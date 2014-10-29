005cb58637a219f1bf6fc5c7e0868a6ec025b6ae
Pete Brooks <peterossbrooks@gmail.com>
2014-10-29 14:51:32 -0500

    DRY-er solution

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,17 +1,22 @@
 module Complement
   DNA_COMPLEMENTS = { 'G' => 'C',
-                  'C' => 'G',
-                  'T' => 'A',
-                  'A' => 'U' }
+                      'C' => 'G',
+                      'T' => 'A',
+                      'A' => 'U' }
   RNA_COMPLEMENTS = DNA_COMPLEMENTS.invert
 
   def self.of_dna(strand)
-    trans = strand.split('').map { |nuc| DNA_COMPLEMENTS[nuc] }
-    trans.join
+    transcribe(strand, DNA_COMPLEMENTS)
   end
 
   def self.of_rna(strand)
-    trans = strand.split('').map { |nuc| RNA_COMPLEMENTS[nuc] }
+    transcribe(strand, RNA_COMPLEMENTS)
+  end
+
+  private
+
+  def self.transcribe(strand, complements_hash)
+    trans = strand.split('').map { |nuc| complements_hash[nuc] }
     trans.join
   end
 end
